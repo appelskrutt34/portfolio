585e2efa621ac8e2f7dcd5aa3093d22e923a08a2
appelskrutt34 <ebba1430@hotmail.com>
2023-12-23 19:24:19 +0100

    fixed sources

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -111,11 +111,11 @@
         <h1 class="font-header">Hobby Projects</h1>
         <h2>Some Websites and Apps I've Crafted and Contributed To</h2>
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 py-20">
-            <Project image="/pages/njord.png" title="Njord.rs" description="Made with Svelte"></Project>
-            <Project image="/pages/explorer.png" title="Kryptokrona Explorer"></Project>
-            <Project image="/pages/kryptokrona.png" title="Kryptokrona Website"></Project>
-            <Project image="/pages/ath.png" title="ATH Games" description="Made with Vue"></Project>
-            <Project image="/pages/kotlinSDK.png" title="Kryptokrona Kotlin SDK" description="Made with Svelte"></Project>
+            <Project image="/pages/njord.png" title="Njord.rs" description="Made with Svelte" source="https://www.njord.rs"></Project>
+            <Project image="/pages/explorer.png" title="Kryptokrona Explorer" source="https://www.explorer.kryptokrona.org"></Project>
+            <Project image="/pages/kryptokrona.png" title="Kryptokrona Website" source="https://www.kryptokrona.org"></Project>
+            <Project image="/pages/ath.png" title="ATH Games" description="Made with Vue" source="https://www.ath.games"></Project>
+            <Project image="/pages/kotlinSDK.png" title="Kryptokrona Kotlin SDK" description="Made with Svelte" source="https://sdk.kryptokrona.org/"></Project>
         </div>
     </div>
     <img src="/wallpapers/geometric.jpg" alt="" class="absolute top-0 left-0 h-full w-full object-cover opacity-20 grayscale">
